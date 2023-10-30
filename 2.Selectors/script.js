1eb7d75b70389e3ee61db70aac075ede64065431
Jana2xN <jana.neirinck@hotmail.com>
2023-10-30 16:18:00 +0100

    Select all the img tags and loop through them. If they have no important class, turn their display property to none

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -2,3 +2,9 @@ const importantElements = document.querySelectorAll(".important");
 importantElements.forEach(element => {
     element.setAttribute("title", "This is an important item");
 });
+const imgElements = document.querySelectorAll("img");
+imgElements.forEach(img => {
+    if (!img.classList.contains("important")) {
+        img.style.display = "none";
+    }
+});

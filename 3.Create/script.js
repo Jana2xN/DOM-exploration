f7a656437a66c6400be51ad751278e780330276a
Jana2xN <jana.neirinck@hotmail.com>
2023-10-31 14:07:38 +0100

    add array of learners with their name and add the function to generate background color

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,12 @@
 /*
 */
+// Array of learners with their names
+const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"];
+
+// Function to generate a random background color
+function getRandomBackgroundColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+  }

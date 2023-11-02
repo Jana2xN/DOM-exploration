741f79cc9a7c2694eb785dd9e93f0510a5de0b30
Jana2xN <jana.neirinck@hotmail.com>
2023-11-02 14:48:18 +0100

    add inserts

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -5,3 +5,12 @@ const ol = document.querySelector("ol");
 const lastLi = ol.lastElementChild;
 const firstLi = ol.firstElementChild;
 ol.insertBefore(lastLi, firstLi);
+
+//Move the <h2> of the third section in the second one and vice-versa
+const secondSection = document.querySelectorAll("section")[1];
+const thirdSection = document.querySelectorAll("section")[2];
+const h2Second = secondSection.querySelector("h2");
+const h2Third = thirdSection.querySelector("h2");
+
+secondSection.insertBefore(h2Third, secondSection.firstChild);
+thirdSection.insertBefore(h2Second, thirdSection.firstChild);

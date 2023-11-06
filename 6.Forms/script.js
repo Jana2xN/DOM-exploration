f0d42e64e7accaa05224e80422eb585e93e6e6e1
Jana2xN <jana.neirinck@hotmail.com>
2023-11-06 08:59:21 +0100

    Add a keyup listener on the first input field

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,2 +1,11 @@
 /*
 */
+// Get the input and span elements
+const firstNameInput = document.getElementById('firstname');
+const displayFirstName = document.getElementById('display-firstname');
+
+// Add a keyup event listener to the input field
+firstNameInput.addEventListener('keyup', () => {
+  // Update the content of the span with the input value
+  displayFirstName.textContent = firstNameInput.value;
+});

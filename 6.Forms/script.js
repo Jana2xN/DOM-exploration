f6a280662c01bfc76ca9e2ac1af3a5296e2aed11
Jana2xN <jana.neirinck@hotmail.com>
2023-11-06 09:15:45 +0100

    Add a keyup listener on the second input

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -9,3 +9,22 @@ firstNameInput.addEventListener('keyup', () => {
   // Update the content of the span with the input value
   displayFirstName.textContent = firstNameInput.value;
 });
+
+// Get the age input and the a-hard-truth section
+const ageInput = document.getElementById('age');
+const aHardTruthSection = document.getElementById('a-hard-truth');
+
+// Add a keyup event listener to the age input
+ageInput.addEventListener('keyup', () => {
+  // Get the value of the age input and convert it to a number
+  const age = parseInt(ageInput.value, 10);
+
+  // Check if age is below 18
+  if (age < 18) {
+    // Hide the a-hard-truth section
+    aHardTruthSection.style.visibility = 'hidden';
+  } else {
+    // Show the a-hard-truth section
+    aHardTruthSection.style.visibility = 'visible';
+  }
+});

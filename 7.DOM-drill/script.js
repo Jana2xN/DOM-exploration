c9e3d9ab8cb31d38bc14d5d298964258338e065d
Jana2xN <jana.neirinck@hotmail.com>
2023-11-06 16:02:47 +0100

    Modify the previous function so that when you press the letter d of your keyboard, the Fast and Furious element gets cloned

--- 7.DOM-drill/script.js
+++ 7.DOM-drill/script.js
@@ -1,54 +1,61 @@
-// Get the <ul> element
-const ulElement = document.querySelector('ul');
-
-// Use childNodes to list all the children from the <ul>
-const childNodes = ulElement.childNodes;
-
-// Create an array to store the list items
-const listItems = [];
-
-// Iterate over the child nodes
-for (let i = 0; i < childNodes.length; i++) {
-  const child = childNodes[i];
-
-  // Use a condition to only iterate over element nodes
-  if (child.nodeType === 1) {
-    listItems.push(child);
-    // Find the element that contains "Fast and Furious"
-    if (child.textContent.includes("Fast and Furious")) {
-      // If it's not the first element, move it to the top of the list
-      if (i > 0) {
-        ulElement.insertBefore(child, ulElement.firstElementChild);
+document.addEventListener("DOMContentLoaded", function () {
+    const ul = document.querySelector("ul");
+  
+    // Create an array to store the unique franchises
+    const uniqueFranchises = [];
+  
+    // Iterate over child nodes of the <ul> element
+    for (let i = 0; i < ul.childNodes.length; i++) {
+      const child = ul.childNodes[i];
+  
+      // Check if the child is an element node
+      if (child.nodeType === 1) {
+        // Check if the text content of the child already exists in the uniqueFranchises array
+        if (uniqueFranchises.includes(child.textContent)) {
+          // If it's a duplicate, remove it
+          ul.removeChild(child);
+        } else {
+          // Add it to the uniqueFranchises array
+          uniqueFranchises.push(child.textContent);
+  
+          // Add a click event listener to each list item
+          child.addEventListener("click", function () {
+            if (child.textContent === "Fast and Furious") {
+              alert(
+                "The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about cars, it's about family."
+              );
+            } else {
+              alert(child.textContent);
+            }
+          });
+  
+          // Check if the element contains "Fast and Furious" and it's not the first element
+          if (child.textContent === "Fast and Furious" && i !== 0) {
+            ul.removeChild(child); // Remove the element
+            ul.insertBefore(child, ul.firstChild); // Move it to the top
+            child.classList.add("important"); // Add the .important class
+          }
+        }
       }
-      // Add a class .important to the element
-      child.classList.add('important');
     }
-    // Add an eventListener on every item of the list
-    child.addEventListener('click', function () {
-      // If the clicked item is "Fast and Furious", show a special message
-      if (child.textContent.includes("Fast and Furious")) {
-        alert("The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about cars, it's about family.");
-      } else {
-        // Otherwise, display the name of the clicked element
-        alert(child.textContent);
+  
+    // Add a keyup event listener to the document body for sorting and cloning
+    document.body.addEventListener("keyup", function (event) {
+      if (event.key === "r") {
+        // Randomly shuffle the list items while keeping "Fast and Furious" first
+        const items = Array.from(ul.children);
+        const fastAndFurious = items.shift();
+        items.sort(() => Math.random() - 0.5);
+        items.unshift(fastAndFurious);
+        ul.innerHTML = ""; // Clear the list
+        items.forEach(function (item) {
+          ul.appendChild(item);
+        });
+      } else if (event.key === "d") {
+        // Clone the "Fast and Furious" element
+        const fastAndFurious = ul.querySelector(".important");
+        const clone = fastAndFurious.cloneNode(true);
+        ul.insertBefore(clone, ul.firstChild);
       }
     });
-  }
-}
-
-// Remove duplicates using removeChild
-const uniqueListItems = listItems.filter((item, index) => listItems.indexOf(item) === index);
-ulElement.innerHTML = '';
-uniqueListItems.forEach((item) => ulElement.appendChild(item));
-
-// Add an eventListener on the document body for keyup
-document.body.addEventListener('keyup', (event) => {
-  // Check if the key pressed is 'r'
-  if (event.key === 'r') {
-    // Sort the list in random order, but ensure "Fast and Furious" is the first element
-    ulElement.innerHTML = ''; // Clear the list
-    uniqueListItems.sort(() => Math.random() - 0.5); // Shuffle the list
-    ulElement.appendChild(listItems.find((item) => item.textContent.includes("Fast and Furious"))); // Ensure "Fast and Furious" is first
-    uniqueListItems.forEach((item) => ulElement.appendChild(item));
-  }
-});
+  });

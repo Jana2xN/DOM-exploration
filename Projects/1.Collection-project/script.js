9363cc01f74d1b78af0a68773cf2923543db2536
Jana2xN <jana.neirinck@hotmail.com>
2023-11-07 15:15:35 +0100

    Create a const collection

---
+++ Projects/1.Collection-project/script.js
@@ -0,0 +1,11 @@
+const collection = [
+    {
+      picture: 'link/to/a/picture',
+      name: 'Doctor Who',
+      releaseYear: 1963,
+      genre: ['Drama', 'Sci-Fi', 'Adventure'],
+      Synopsis: ['The further adventures in time and space of the alien adventures known as the Doctor and their companions from planet earth.']
+    },
+  
+    // ...
+  ]
